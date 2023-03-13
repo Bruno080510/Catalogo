770800d0584253908649a0c95a919e1001d7360f
burnolealmaciel <burnolealmaciel@gmail.com>
2023-03-13 11:19:42 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,63 +1,26 @@
 let inputBuscarFilme = document.querySelector("#input-buscar-filme")
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme")
+console.log(btnBuscarFilme.innerHTML)
 
-btnBuscarFilme.onclick = () => {
-    if(inputBuscarFilme.value.length > 0){
-            console.log(inputBuscarFilme.value)
-    }
-    return false
-}
-
-btnBuscarFilme.onclick = () => {
-    if(inputBuscarFilme.value.length > 0){
-        fetch("http://www.omdbapi.com/?apikey=323c70d9&s="+inputBuscarFilme.value, {mode: "cors"})
-        .then((resp)=> resp.json())
-        .then((resp)=> {
-            console.log(resp)
+let listarFilmes = async (filmes) => {
+    let listaFilmes = await document.querySelector("#lista-filmes")
+    listaFilmes.innerHTML = ""
+    console.log(listaFilmes)
+    if(filmes.length > 0){
+        filmes.forEach(async(filme) => {
+            listaFilmes.appendChild(await filme.getCard())
         })
     }
-    return false
 }
 
-getCard = async () => {
-    let card = document.createElement("div")
-    card.setAttribute("class", "card")
-    let imgCartaz = document.createElement("img")
-    imgCartaz.setAttribute("class", "card-img-topz")
-    imgCartaz.setAttribute("src", this.cartaz)
-    let cardBody = document.createElement("div")
-    cardBody.setAttribute("class", "card-body")
-    let hCardTitle = document.createElement("h5")
-    hCardTitle.setAttribute("class", "card-title")
-    let divDetalhes = document.createElement("div")
-    divDetalhes.setAttribute("style","display:flex; justify-content:space-around;")
-    let divGenero = document.createElement("div")
-    divGenero.setAttribute("style", "flex-grow:1;")
-    let divAnoProducao = document.createElement("div")
-    divAnoProducao.setAttribute("style", "flex-grow:1;")
-    let divClassificacao = document.createElement("div")
-    divClassificacao.setAttribute("style", "flex-grow:1;")
-    hCardTitle.appendChild(document.createTextNode(this.titulo))
-    divGenero.appendChild(document.createTextNode(this.genero))
-    divAnoProducao.appendChild(document.createTextNode(this.ano))
-    divClassificacao.appendChild(document.createTextNode(this.classificacao))
-    divDetalhes.appendChild(divGenero)
-    divDetalhes.appendChild(divAnoProducao)
-    divDetalhes.appendChild(divClassificacao)
-    card.appendChild(imgCartaz)
-    card.appendChild(cardBody)
-    cardBody.appendChild(hCardTitle)
-    cardBody.appendChild(divDetalhes)
-    return card
-}
-
-btnBuscarFilme.onclick = async () => {
+btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
         let filmes =  new Array()
-        fetch("http://www.omdbapi.com/?apikey=323c70d9&s="+ inputBuscarFilme.value)
+        fetch("http://www.omdbapi.com/?apikey=323c70d9&s="+inputBuscarFilme.value)
         .then((resp) => resp.json())
         .then((resp)=> {
             resp.Search.forEach((item) => {
+                console.log(item)
                 let filme=new Filme(
                     item.imdbId,
                     item.Title,
@@ -79,13 +42,3 @@ btnBuscarFilme.onclick = async () => {
     return false
 }
 
-let listarFilmes = async (filmes) => {
-    let listaFilmes = await document.querySelector("#lista-filmes")
-    listaFilmes.innerHTML = ""
-    console.log(listaFilmes)
-    if(filmes.length > 0){
-        filmes.forEach(async(filme) => {
-            listaFilmes.appendChild(await filme.getCard())
-        })
-    }
-}
